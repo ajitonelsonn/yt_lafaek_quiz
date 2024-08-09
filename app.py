bc15b71127650e14cbf6989c9e682b5c1743ad9a
ajito nelson <8997ajito@gmail.com>
2024-08-09 12:03:50 +0900

    make streamlit app

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Title of the app
+st.title('Hello World Streamlit App')
+
+# Displaying a simple text
+st.write('Hello, world!')
+
+# Running the app
+if __name__ == '__main__':
+    st.run()
